dd9f909daa8a027653bde10e6eb2de1ce8850b3b
Matthew Miller <mattdm@mattdm.org>
2016-06-06 10:48:46 -0400

    rudimentary framework works. doesn't actually calculate or save info

--- 2015-12-02/weekly-user-activity.py
+++ 2015-12-02/weekly-user-activity.py
@@ -7,16 +7,21 @@ import fedmsg.config
 config = fedmsg.config.load_config()
 fedmsg.meta.make_processors(**config)
 
+
+
 import time
 import datetime
 import logging
 import os
 import sys
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.ERROR)
+
+epoch = datetime.datetime.utcfromtimestamp(0)
 
 discriminant = sys.argv[-1]
 if __file__.split('/')[-1] in discriminant:
-    print "usage: '$ ./active-users-per-week.py TOPIC'"
+    print "usage: '$ ./weekly-user-activity.py TOPIC'"
     sys.exit(1)
 
 print "operating with discriminant", discriminant
@@ -33,44 +38,43 @@ try:
 except OSError:
     pass
 
-#for year in [2015, 2016]:
-for year in [2012, 2013, 2014, 2015, 2016]:
+starttime = datetime.datetime.strptime("2012-01-01", "%Y-%m-%d")
+
+while starttime < datetime.datetime.now():
+    endtime   = starttime + datetime.timedelta(7)
+
+    print "Working on %s / %s" % (discriminant, starttime.strftime("%Y-%m-%d"))
+
+    messages = utils.grep(
+        rows_per_page=100,
+        meta='usernames',
+        start=int((starttime-epoch).total_seconds()),
+        end=int((endtime - epoch).total_seconds()),
+        order='asc',  # Start at the beginning, end at now.
+        topic=discriminant,
+        # Cut this stuff out, because its just so spammy.
+        not_user=['koschei', 'anonymous'],
+        not_topic=verboten,
+    )
+
+    users = {}
+    for i, msg in enumerate(messages):
+        # sanity check
+        if msg['topic'] in verboten:
+            raise "hell"
+
+        for user in msg['meta']['usernames']:
+            users[user] = users.get(user, 0) + 1
+
+        if i % 50 == 0:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+
+    print " done reading", starttime.strftime("%Y-%m-%d")
+
+    for user in users:
+        print 'User: %20s  Actions: %4i' % (user, users[user])
+
+    # and loop around
+    starttime=endtime
     
-    for day in range(8, 365, 7):
-        bucket = "{0}-{1:0>3}".format(year, day - 7)
-        terminus = "{0}-{1:0>3}".format(year, day)
-        start = int(time.mktime(datetime.datetime.strptime(bucket, "%Y-%j").timetuple()))
-        end = int(time.mktime(datetime.datetime.strptime(terminus, "%Y-%j").timetuple()))
-
-        print "Working on data/%s/%s.csv" % (discriminant, bucket)
-
-        messages = utils.grep(
-            rows_per_page=100,
-            meta='usernames',
-            start=start,
-            end=end,
-            order='asc',  # Start at the beginning, end at now.
-            topic=discriminant,
-            # Cut this stuff out, because its just so spammy.
-            not_user=['koschei', 'anonymous'],
-            not_topic=verboten,
-        )
-
-        users = {}
-        for i, msg in enumerate(messages):
-            # sanity check
-            if msg['topic'] in verboten:
-                raise "hell"
-
-            for user in msg['meta']['usernames']:
-                users[user] = users.get(user, 0) + 1
-
-            if i % 50 == 0:
-                sys.stdout.write(".")
-                sys.stdout.flush()
-
-        print " done with", bucket
-
-        with open('data/%s/%s.csv' % (discriminant, bucket), 'w') as f:
-            for user in users:
-                f.write('%s, %i\n' % (user, users[user]))
