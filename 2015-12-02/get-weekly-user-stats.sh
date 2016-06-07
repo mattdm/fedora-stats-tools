147a067d69a373adba7b69bccedcb655681aaee0
Matthew Miller <mattdm@mattdm.org>
2016-06-07 15:42:02 -0400

    run all three

---
+++ 2015-12-02/get-weekly-user-stats.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#./weekly-user-activity.py org.fedoraproject.prod.git.receive
+./weekly-user-activity.py org.fedoraproject.prod.bodhi.update.comment
+./weekly-user-activity.py org.fedoraproject.prod.wiki.article.edit.csv
