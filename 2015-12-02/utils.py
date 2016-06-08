e0d0064aec709b625d0d21386bd9b60c5cddbb65
Matthew Miller <mattdm@mattdm.org>
2016-06-08 18:32:11 -0400

    add retrying

--- 2015-12-02/utils.py
+++ 2015-12-02/utils.py
@@ -30,16 +30,24 @@ def grep(tries=0, **kwargs):
         yield message
 
     for page in range(1, pages):
-        kwargs['page'] = page
-        response = requests.get(url, params=kwargs)
-        try:
-            data = response.json()
-        except Exception as e:
-            if "Expecting value" in str(e):
-                continue
+        for attempt in range(10):
+            try:
+                kwargs['page'] = page
+                response = requests.get(url, params=kwargs)
+                try:
+                    data = response.json()
+                except Exception as e:
+                    if "Expecting value" in str(e):
+                        continue
+                    else:
+                        raise
+            except ValueError as error:
+                print "Value Error in json.. retrying %s" % (attempt)
+                print error
             else:
-                raise
-
+                break
+        else:
+            raise ValueError("Ran out of retries")
         for message in data.get('raw_messages', []):
             if badge_id(message) in ignored_badges:
                 continue
