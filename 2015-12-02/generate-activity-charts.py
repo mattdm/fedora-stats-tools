c87fc17a92bde4034a4dea2b443453d33e29c6e3
Matthew Miller <mattdm@mattdm.org>
2016-07-31 10:08:28 +0200

    update with bodhi

--- 2015-12-02/generate-activity-charts.py
+++ 2015-12-02/generate-activity-charts.py
@@ -6,10 +6,10 @@ m.rcParams['font.size'] = 12
 m.rcParams['font.family'] = 'Overpass'
 m.rcParams['legend.frameon'] = False
 
-data=pandas.read_csv("data/org.fedoraproject.prod.git.receive.bucketed-activity.csv",parse_dates=[0])
-data.set_index('weekstart',inplace=True)
+datagit=pandas.read_csv("data/org.fedoraproject.prod.git.receive.bucketed-activity.csv",parse_dates=[0])
+datagit.set_index('weekstart',inplace=True)
 
-graph=data[['users1','users9','users40','userrest']].rename(columns={"users1": "Top 1%","users9":"Top 9%","users40":"Top 40%","userrest":"Remaining 50%"}).plot.area(figsize=(16, 9),
+graph=datagit[['users1','users9','users40','userrest']].rename(columns={"users1": "Top 1%","users9":"Top 9%","users40":"Top 40%","userrest":"Remaining 50%"}).plot.area(figsize=(16, 9),
                                                               color=['#579d1c','#ffd320', '#ff420e', '#004586' ],
                                                               grid=True,yticks=range(0,301,25))
 #graph.legend(ncol=4)
@@ -22,17 +22,17 @@ fig.savefig('images/git.user.count.svg',dpi=300)
 
 #############################################
 
-data['msgstotal']=data[['msgs1','msgs9','msgs40','msgsrest']].sum(1)
-data['msgs1%']=100*data['msgs1']/data['msgstotal']
-data['msgs9%']=100*data['msgs9']/data['msgstotal']
-data['msgs40%']=100*data['msgs40']/data['msgstotal']
-data['msgsrest%']=100*data['msgsrest']/data['msgstotal']
+datagit['msgstotal']=datagit[['msgs1','msgs9','msgs40','msgsrest']].sum(1)
+datagit['msgs1%']=100*datagit['msgs1']/datagit['msgstotal']
+datagit['msgs9%']=100*datagit['msgs9']/datagit['msgstotal']
+datagit['msgs40%']=100*datagit['msgs40']/datagit['msgstotal']
+datagit['msgsrest%']=100*datagit['msgsrest']/datagit['msgstotal']
 
 
 
 
 m.rcParams['legend.frameon'] = True
-graph=data[['msgs1%','msgs9%','msgs40%','msgsrest%']].rename(columns={"msgs1%": "Top 1%","msgs9%":"Top 9%","msgs40%":"Top 40%","msgsrest%":"Remaining 50%"}).plot.area(figsize=(16, 9),
+graph=datagit[['msgs1%','msgs9%','msgs40%','msgsrest%']].rename(columns={"msgs1%": "Top 1%","msgs9%":"Top 9%","msgs40%":"Top 40%","msgsrest%":"Remaining 50%"}).plot.area(figsize=(16, 9),
                                                               color=['#579d1c','#ffd320', '#ff420e', '#004586' ],
                                                               grid=True,ylim=(0,100))
 plt.suptitle("Percent of Package Changes Each Week From Each Activity Level Group",fontsize=24)
@@ -44,10 +44,10 @@ fig.savefig('images/git.activity.share.svg',dpi=300)
 
 ###############################################
 
-graph=data[['newusercount']].rename(columns={"newusercount": "New Users"}).plot.area(figsize=(16, 9),
+graph=datagit[['newusercount']].rename(columns={"newusercount": "New Users"}).plot.area(figsize=(16, 9),
                                                               color='#579d1c',
                                                               grid=True,legend=False)
-plt.suptitle("New Contributor Count Per Week",fontsize=24)
+plt.suptitle("New Packaging Contributor Count Per Week",fontsize=24)
 graph.set_title('')
 graph.set_xlabel('')
 fig=graph.get_figure()
@@ -55,16 +55,16 @@ fig.savefig('images/git.newusers.svg',dpi=300)
 
 #############################################
 
-data['newuseractions%']=100*data['newuseractions']/data['msgstotal']
-data['monthuseractions%']=100*data['monthuseractions']/data['msgstotal']
-data['yearuseractions%']=100*data['yearuseractions']/data['msgstotal']
-data['olderuseractions%']=100*data['olderuseractions']/data['msgstotal']
+datagit['newuseractions%']=100*datagit['newuseractions']/datagit['msgstotal']
+datagit['monthuseractions%']=100*datagit['monthuseractions']/datagit['msgstotal']
+datagit['yearuseractions%']=100*datagit['yearuseractions']/datagit['msgstotal']
+datagit['olderuseractions%']=100*datagit['olderuseractions']/datagit['msgstotal']
 
 
 
 
 m.rcParams['legend.frameon'] = True
-graph=data[['newuseractions%','monthuseractions%','yearuseractions%','olderuseractions%']][42:].rename(columns={"newuseractions%": "New This Week","monthuseractions%":"New This Month","yearuseractions%":"New This Year","olderuseractions%":"Old School"}).plot.area(figsize=(16, 9),
+graph=datagit[['newuseractions%','monthuseractions%','yearuseractions%','olderuseractions%']][42:].rename(columns={"newuseractions%": "New This Week","monthuseractions%":"New This Month","yearuseractions%":"New This Year","olderuseractions%":"Old School"}).plot.area(figsize=(16, 9),
                                                               color=['#579d1c','#ffd320', '#ff420e', '#004586' ],
                                                               grid=True,ylim=(0,100))
 plt.suptitle("Percent of Package Changes Each Week By Time Since Packager's First Action",fontsize=24)
@@ -73,3 +73,75 @@ graph.set_xlabel('')
 
 fig=graph.get_figure()
 fig.savefig('images/git.activity.length.svg',dpi=300)
+
+################################################################################################################
+################################################################################################################
+
+databodhi=pandas.read_csv("data/org.fedoraproject.prod.bodhi.update.comment.bucketed-activity.csv",parse_dates=[0])
+databodhi.set_index('weekstart',inplace=True)
+
+graph=databodhi[['users1','users9','users40','userrest']].rename(columns={"users1": "Top 1%","users9":"Top 9%","users40":"Top 40%","userrest":"Remaining 50%"}).plot.area(figsize=(16, 9),
+                                                              color=['#579d1c','#ffd320', '#ff420e', '#004586' ],
+                                                              grid=True,yticks=range(0,301,25))
+#graph.legend(ncol=4)
+# totally abusing this.
+plt.suptitle("Number of Contributors Providing Feedback on Package Updates Each Week",fontsize=24)
+graph.set_title("Grouped by Quarterly Activity Level of Each Contributor",fontsize=16)
+graph.set_xlabel('')
+fig=graph.get_figure()
+fig.savefig('images/bodhi.user.count.svg',dpi=300)
+
+#############################################
+
+databodhi['msgstotal']=databodhi[['msgs1','msgs9','msgs40','msgsrest']].sum(1)
+databodhi['msgs1%']=100*databodhi['msgs1']/databodhi['msgstotal']
+databodhi['msgs9%']=100*databodhi['msgs9']/databodhi['msgstotal']
+databodhi['msgs40%']=100*databodhi['msgs40']/databodhi['msgstotal']
+databodhi['msgsrest%']=100*databodhi['msgsrest']/databodhi['msgstotal']
+
+
+
+
+m.rcParams['legend.frameon'] = True
+graph=databodhi[['msgs1%','msgs9%','msgs40%','msgsrest%']].rename(columns={"msgs1%": "Top 1%","msgs9%":"Top 9%","msgs40%":"Top 40%","msgsrest%":"Remaining 50%"}).plot.area(figsize=(16, 9),
+                                                              color=['#579d1c','#ffd320', '#ff420e', '#004586' ],
+                                                              grid=True,ylim=(0,100))
+plt.suptitle("Percent of Update Feedback Each Week From Each Activity Level Group",fontsize=24)
+graph.set_title("",fontsize=16)
+graph.set_xlabel('')
+
+fig=graph.get_figure()
+fig.savefig('images/bodhi.activity.share.svg',dpi=300)
+
+###############################################
+
+graph=databodhi[['newusercount']].rename(columns={"newusercount": "New Users"}).plot.area(figsize=(16, 9),
+                                                              color='#579d1c',
+                                                              grid=True,legend=False)
+plt.suptitle("New Update Testing Contributor Count Per Week",fontsize=24)
+graph.set_title('')
+graph.set_xlabel('')
+fig=graph.get_figure()
+fig.savefig('images/bodhi.newusers.svg',dpi=300)
+
+#############################################
+
+databodhi['newuseractions%']=100*databodhi['newuseractions']/databodhi['msgstotal']
+databodhi['monthuseractions%']=100*databodhi['monthuseractions']/databodhi['msgstotal']
+databodhi['yearuseractions%']=100*databodhi['yearuseractions']/databodhi['msgstotal']
+databodhi['olderuseractions%']=100*databodhi['olderuseractions']/databodhi['msgstotal']
+
+
+
+
+m.rcParams['legend.frameon'] = True
+graph=databodhi[['newuseractions%','monthuseractions%','yearuseractions%','olderuseractions%']][42:].rename(columns={"newuseractions%": "New This Week","monthuseractions%":"New This Month","yearuseractions%":"New This Year","olderuseractions%":"Old School"}).plot.area(figsize=(16, 9),
+                                                              color=['#579d1c','#ffd320', '#ff420e', '#004586' ],
+                                                              grid=True,ylim=(0,100))
+plt.suptitle("Percent of Update Feedback Each Week By Time Since Packager's First Action",fontsize=24)
+graph.set_title("",fontsize=16)
+graph.set_xlabel('')
+
+fig=graph.get_figure()
+fig.savefig('images/bodhi.activity.length.svg',dpi=300)
+
